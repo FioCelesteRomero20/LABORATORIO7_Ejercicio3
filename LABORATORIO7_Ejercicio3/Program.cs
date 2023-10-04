06e4a00cbf79d0426493e37b097c8e47e7f7d1b5
PC <PC@DESKTOP-NOE8GP1>
2023-10-04 18:33:32 -0500

    Agregar archivos de proyecto.

---
+++ LABORATORIO7_Ejercicio3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABORATORIO7_Ejercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ingresar datos
+            int i;
+
+            for (i = 1900; i <= 2023; i++)
+            {
+                if (i % 4 == 0)
+                {
+                    //Detectar años bisiestos
+                    Console.WriteLine(i);
+                    // numero = numero + i;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
